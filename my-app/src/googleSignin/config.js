34ac6ed825a0e769059aedd10e35224b383d995d
Wahyu Eko <wahyu.ehs@gmail.com>
2023-05-26 20:16:46 +0700

    ok

--- my-app/src/googleSignin/config.js
+++ my-app/src/googleSignin/config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider} from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyDgiX1H6oZOB8-T6cFtVE50UsvrkgtBmxw",
-  authDomain: "wahyu-project-e6e3b.firebaseapp.com",
-  projectId: "wahyu-project-e6e3b",
-  storageBucket: "wahyu-project-e6e3b.appspot.com",
-  messagingSenderId: "19142158975",
-  appId: "1:19142158975:web:52508425be4915f276ad4a"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
--- my-app/src/signGoo/firebase.js
+++ my-app/src/signGoo/firebase.js
@@ -19,12 +19,12 @@ import {
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDgiX1H6oZOB8-T6cFtVE50UsvrkgtBmxw",
-  authDomain: "wahyu-project-e6e3b.firebaseapp.com",
-  projectId: "wahyu-project-e6e3b",
-  storageBucket: "wahyu-project-e6e3b.appspot.com",
-  messagingSenderId: "19142158975",
-  appId: "1:19142158975:web:52508425be4915f276ad4a"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
