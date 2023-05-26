6c5722b2520f418acbc2c9746d8bc6c1b758ea93
Wahyu Eko <wahyu.ehs@gmail.com>
2023-05-26 18:40:07 +0700

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from './signGoo/Login';
-import Register from './signGoo/register';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
         </Routes>
       </Router>
     </div>
